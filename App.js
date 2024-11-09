03fa007f9c84b7c46d75805be1dd380619e2301e
CartiFelipe <143661551+CartiFelipe@users.noreply.github.com>
2024-11-09 16:29:19 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import ProductLog from './src/views/productsLog/Index.jsx';
-import Products from './src/views/products/Index.jsx';
-import Main from "./src/views/main/Index.jsx";
+
+import Primeira from "./src/questoes/primera/Index.jsx";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Main />
+      <Primeira />
 
 
     </SafeAreaView>
